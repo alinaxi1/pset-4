d62efbee11cf778354d04d9c509419c81db3625c
Alina Xi <AXi@ucvts.tec.nj.us>
2019-11-19 10:50:23 -0500

    hjafbklawdudfh

--- src/average.js
+++ src/average.js
@@ -3,9 +3,19 @@ const readlineSync = require("readline-sync");
 const MIN = 0;
 const MAX = Number.MAX_SAFE_INTEGER;
 
-let negative = Number(readlineSync.question("\nNon-negative Integer: "));
 let sum = 0;
+let num = 0;
+let value = Number(readlineSync.question("\nNon-negative Integer: "));
 
-while (Number.isNaN(negative) || negative < MIN || negative > MAX || !Number.isInteger(negative)) {
-  negative = Number(readlineSync.question("\nNon-negative Integer: "));
+while (value !== -1 && value > MIN && value < MAX && Number.isInteger(value)) {
+  value = Number(readlineSync.question("Non-negative Integer: "));
+
+  if (value !== -1) {
+      sum = sum + value;
+      num++
+  }
 }
+sum = sum / num;
+sum = sum.toFixed(3);
+sum = sum.toLocaleString('en');
+console.log("\n" + sum);
