114019a27a6324ecfe24a77b495e81e42b197b6c
alinaxi1 <55397901+alinaxi1@users.noreply.github.com>
2019-11-23 19:01:30 -0500

    done with 7

--- src/factors.js
+++ src/factors.js
@@ -4,19 +4,18 @@ const MIN = 1;
 const MAX = Number.MAX_SAFE_INTEGER;
 
 let num = Number(readlineSync.question("\nPositive integer: "));
-var factor = "0";
+let factor = 0;
 
 while (Number.isNaN(num) || num < MIN || num > MAX || !Number.isInteger(num)) {
   num = Number(readlineSync.question("Positive integer: "));
 }
 
 process.stdout.write("\n");
-var str = "0";
 
-for (var i = 1; i <= num; i++) {
-    if (num % i == 0) {
-        str += ',' + i;
-    }
+for (var i = 1; i * i <= num; i++) {
+  if (num % i == 0)  {
+    factors = (i == (num / i)) ? i + ".\n " : i + ", " + num / i + ", "
+    process.stdout.write(factors);
+  }
+
 }
-console.log(str + ".");
-return (str);
